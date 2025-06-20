107064096e6039da1898fab21ba697d05be69a83
ImNotForq <neon20.xr@gmail.com>
2025-06-20 21:31:27 +0400

    added support for media and albums

--- plugin/reposter.py
+++ plugin/reposter.py
@@ -1,13 +1,14 @@
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.types import PeerChannel
 from telethon import TelegramClient, events
-from telegram import Bot
+from collections import deque
 
 import re
-import asyncio
 import dotenv
 
 
 config = dotenv.dotenv_values(".env")
-
+processed_albums = deque(maxlen=10)
 
 api_id = config["api_id"]
 api_hash = config["api_hash"]
@@ -17,29 +18,50 @@ target_chat = config["target_chat"]
 link_replaced = config["link_replaced"]
 name_replaced = config["name_replaced"]
 
-client = TelegramClient('session', api_id, api_hash)
+client = TelegramClient('session', api_id, api_hash, connection_retries=5, timeout=10)
+
+async def resolve_chat_by_id(client, chat_id: int):
+    peer = PeerChannel(chat_id)
+    full = await client(GetFullChannelRequest(peer))
+    return full.chats[0]
+
 
 @client.on(events.NewMessage(chats=source_channel))
-async def handler(event):
-    final_message = re.sub(r'\[.*?\]\(https://t\.me/\S+?\)', f'[{name_replaced}]({link_replaced})', event.message.text)
+async def message_handler(event: events.NewMessage.Event):
     try:
+
+        if event.message.grouped_id is not None:
+            if event.message.grouped_id in processed_albums:
+                return
+            else:
+                processed_albums.append(event.message.grouped_id)
+                return
+
+        chat_id = int(config["target_chat"])
+        target_chat = await resolve_chat_by_id(client, chat_id)
+
+        text = event.message.text or ""
+        final_message = re.sub(r'\[.*?\]\(https://t\.me/\S+?\)', f'[{name_replaced}]({link_replaced})', text)
         if event.message.media:
-            await client.send_file(int(target_chat), file=event.message.media, caption=final_message)
+            await client.send_file(target_chat, file=event.message, caption=final_message)
         else:
-            await client.send_message(int(target_chat), final_message)
+            await client.send_message(target_chat, final_message)
 
         print(f"{event.message.text}")
-        print("Message forwarded")
+
     except Exception as e:
-        print(f"Cause an error while forwarding a message: {e}")
+        print(f"{e}")
 
-    chat = await event.get_chat()
-    print(f"Название чата: {chat.title if hasattr(chat, 'title') else 'Личка'}")
-    print(f"ID чата: {chat.id}")
-
-async def main():
-    print("Self bot has runned")
-    await client.run_until_disconnected()
+@client.on(events.Album(chats=source_channel)) 
+async def album_handler(event):
+    text = event.text or ""
+    final_message = re.sub(r'\[.*?\]\(https://t\.me/\S+?\)', f'[{name_replaced}]({link_replaced})', text)
+    target_chat = await resolve_chat_by_id(client, int(config["target_chat"]))
+    await client.send_message(
+        entity=target_chat,
+        file=event.messages,
+        message=final_message,
+    )
 
 client.start()
 client.run_until_disconnected()
